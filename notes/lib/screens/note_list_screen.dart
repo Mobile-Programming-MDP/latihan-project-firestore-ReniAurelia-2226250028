d64ac6a74f4e9eb764eb6ff9604d140f1d5f4ac7
ReniAurelia-2226250028 <137689204+ReniAurelia-2226250028@users.noreply.github.com>
2024-05-16 20:10:54 +0700

    notes

--- notes/lib/screens/note_list_screen.dart
+++ notes/lib/screens/note_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:notes/models/note.dart';
 import 'package:notes/services/note_service.dart';
 import 'package:notes/widgets/note_dialog.dart';
 
@@ -54,31 +56,87 @@ class NoteList extends StatelessWidget {
               padding: const EdgeInsets.only(bottom: 80),
               children: snapshot.data!.map((document) {
                 return Card(
-                  child: ListTile(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return NoteDialog(note: document);
+                  child: Column(
+                    children: [
+                      document.imageUrl != null &&
+                              Uri.parse(document.imageUrl!).isAbsolute
+                          ? ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(16),
+                                topRight: Radius.circular(16),
+                              ),
+                              child: Image.network(
+                                document.imageUrl!,
+                                width: double.infinity,
+                                height: 150,
+                                fit: BoxFit.cover,
+                                alignment: Alignment.center,
+                              ),
+                            )
+                          : Container(),
+                      ListTile(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return NoteDialog(note: document);
+                            },
+                          );
                         },
-                      );
-                    },
-                    title: Text(document.title),
-                    subtitle: Text(document.description),
-                    trailing: InkWell(
-                      onTap: () {
-                        NoteService.deleteNote(document);
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: Icon(Icons.delete),
+                        title: Text(document.title),
+                        subtitle: Text(document.description),
+                        trailing: InkWell(
+                          onTap: () {
+                            showAlertDialog(context, document);
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Icon(Icons.delete),
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
                 );
               }).toList(),
             );
         }
+      },
+    );
+  }
+
+  showAlertDialog(BuildContext context, Note document) {
+    // set up the buttons
+    Widget cancelButton = ElevatedButton(
+      child: const Text("No"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = ElevatedButton(
+      child: const Text("Yes"),
+      onPressed: () {
+        NoteService.deleteNote(document).whenComplete(() {
+          Navigator.of(context).pop();
+        });
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: const Text("Delete Note"),
+      content: const Text("Are you sure to delete Note?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
       },
     );
   }
